828b56a5542eb760e03bd6d16f792c905712fabe
xiaoyi001yeye <xiaoyi001@qq.com>
2018-11-15 09:50:27 +0800

    Create xcall.sh

---
+++ xcall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+params=$@
+pcount=$#
+if (( $pcount < 1 )) ; then
+    echo no args;
+    exit ;
+fi
+for ((i=1 ; i<=5 ; i=$i+1 )) ; do
+    if [ "`hostname`" == "hdp00$i" ] ; then
+        echo ==========localhost hdp00$i $params========
+        $params
+    else
+        echo ============hdp00$i $params============
+        echo
+        ssh hdp00$i "$params"
+        echo
+
+    fi
+done
